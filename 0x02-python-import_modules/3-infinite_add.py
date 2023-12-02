4a28c71bf99552518442608e4754a80e0f7d6893
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-12-02 09:42:19 -0800

    l

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
-
 import sys
 
-length = len(sys.argv)
-summ = 0
 if __name__ == "__main__":
-    if length == 1:
-        summ = 0
-    else:
-        for i in range(1, length):
-            summ += int(sys.argv[i])
-    print("{:d}".format(summ))
+    args = sys.argv[1:]  # Exclude the script name from the arguments list
+    result = 0
+
+    for arg in args:
+        result += int(arg)
+
+    print(result)
